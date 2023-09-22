24b2dac2b1e71d594b60bf6159d286424f2a8e20
MD Mehedi Mostazir <plabonmostazir@gmail.com>
2023-09-22 23:32:20 +0600

    pattern program

--- program3.java
+++ program3.java
@@ -4,9 +4,11 @@ public class program3 {
      //outer loops for handeling 
      for (i = 1; i <= 6; i++){
        
-        for (j = 1; j<= 5 ; j++){
-          System.out.print(" ");
+        for (j = 1; j<= i ; j++){
+
+          System.out.print(" * ");
         }
+        System.out.println();
      }
   }
 }
